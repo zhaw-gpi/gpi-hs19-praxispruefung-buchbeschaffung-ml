90f92eaabd75bfda23b9cdaa15bf0cb9f9ebf460
Björn Scheppler <scep@zhaw.ch>
2019-11-26 16:24:46 +0100

    Refactoring

--- bibverw/src/main/java/ch/zhaw/iwi/gpi/bibverw/BibVerwApplication.java
+++ bibverw/src/main/java/ch/zhaw/iwi/gpi/bibverw/BibVerwApplication.java
@@ -1,13 +1,13 @@
-package ch.zhaw.iwi.gpi.restservice;
+package ch.zhaw.iwi.gpi.bibverw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestServiceApplication {
+public class BibVerwApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
+		SpringApplication.run(BibVerwApplication.class, args);
 	}
 
 }
